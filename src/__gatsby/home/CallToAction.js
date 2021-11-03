4be85fdc389af04a60ff8d15b4cddc484ce3418f
Sean <>
2021-11-03 15:03:34 -0700

    fix for seo purposes

--- src/__gatsby/home/CallToAction.js
+++ src/__gatsby/home/CallToAction.js
@@ -80,8 +80,8 @@ export default function CallToAction() {
               align={matchesMD ? 'center' : undefined}
               variant="body1"
             >
-              At CarX our mission is to provide you with the car shopping
-              experience you deserve.
+              At CarX our mission is to make buying a car easier than ever
+              before.
             </Typography>
           </Grid>
           <Grid
