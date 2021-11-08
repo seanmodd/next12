2bf42d602887fc281facfb192b41513b485a37cc
Sean <>
2021-11-08 12:05:00 -0800

    cleaned up more

--- pages/_app.js
+++ pages/_app.js
@@ -1,74 +1,39 @@
 // mock api
 import '../src/_apis_';
-
-// i18n
-// import '../src/locales/i18n';
-
-// highlight
-// import '../src/utils/highlight';
-// read the blog here: https://sunde.dev/blog/Beautify_your_markdown_Nextjs_blog_with_Highlightjs
-// scroll bar
 import 'simplebar/src/simplebar.css';
-
-// map
 import 'mapbox-gl/dist/mapbox-gl.css';
-
-// lightbox
 import 'react-image-lightbox/style.css';
-
-// editor
 import 'react-quill/dist/quill.snow.css';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-
-// slick-carousel
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
-// lazy image
 import 'lazysizes';
 import 'lazysizes/plugins/attrchange/ls.attrchange';
 import 'lazysizes/plugins/object-fit/ls.object-fit';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-
-// next
 import Head from 'next/head';
 import { CacheProvider } from '@emotion/react';
-// material
 import { NoSsr } from '@mui/material';
-// contexts
 import { SettingsProvider } from 'src/contexts/SettingsContext';
 import { CollapseDrawerProvider } from 'src/contexts/CollapseDrawerContext';
-// theme
 import ThemeConfig from 'src/theme';
 import GlobalStyles from 'src/theme/globalStyles';
-// utils
 import createEmotionCache from 'src/utils/createEmotionCache';
-// components
 import Settings from 'src/components/settings';
 import RtlLayout from 'src/components/RtlLayout';
 import ProgressBar from 'src/components/ProgressBar';
 import LoadingScreen from 'src/components/LoadingScreen';
-
 import ThemePrimaryColor from 'src/components/ThemePrimaryColor';
-
-//! ----------------------------------------------------------------------
-//! all imports below are from index.js full javascript version... not sure about relevancy for next.js for some...
-//! ----------------------------------------------------------------------
 
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider as ReduxProvider } from 'react-redux';
-
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-// redux
 import { persistor, makeStore } from 'src/___redux/store';
-
 import { AuthProvider } from 'src/contexts/FirebaseContext';
 
-//! below is from App.js within javascript version...
 import GoogleAnalytics from 'src/minimalComponents/GoogleAnalytics';
 import NotistackProvider from 'src/minimalComponents/NotistackProvider';
-import ThemeLocalization from 'src/minimalComponents/ThemeLocalization';
 import { BaseOptionChartStyle } from 'src/minimalComponents/charts/BaseOptionChart';
 import {
   UserWrapper,
@@ -76,8 +41,7 @@ import {
   CartWrapper,
 } from 'src/__gatsby/contexts';
 import { ApolloWrapper } from 'src/__graphql/ApolloWrapper';
-//! All imports from full javascript version end here ...
-//! below is from root-wrapper.js within gatsby version...
+
 import { ThemeProvider } from '@material-ui/core/styles';
 import MainLayout from 'src/layouts/main';
 import { wrapperStore } from 'src/___redux/store.js';
