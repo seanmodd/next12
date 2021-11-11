bef24310a64221f50fb1902c1a3a52176bf5f138
Sean <>
2021-11-10 20:24:34 -0800

    good reset point

--- src/___global/components/LoginNotification.js
+++ src/___global/components/LoginNotification.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
-import { LoginNotificationWrapper } from '../elements';
+// import { LoginNotificationWrapper } from '../elements';
 import Context from '../store/context';
 
 export const LoginNotification = () => {
   const { globalState } = useContext(Context);
 
   return (
-    <LoginNotificationWrapper loggedIn={globalState.isLoggedIn}>
+    <>
       <p>Please login to download image</p>
-    </LoginNotificationWrapper>
+    </>
   );
 };
