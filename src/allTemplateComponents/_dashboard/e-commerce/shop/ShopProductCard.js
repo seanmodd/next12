9162d375b0b835b9fbb5deb34cc4c9cb7c473840
Sean <>
2021-12-10 13:07:34 -0800

    just added a box around heart in ShopProductCard.js

--- src/allTemplateComponents/_dashboard/e-commerce/shop/ShopProductCard.js
+++ src/allTemplateComponents/_dashboard/e-commerce/shop/ShopProductCard.js
@@ -1,28 +1,28 @@
-import PropTypes from "prop-types";
-import { paramCase } from "change-case";
+import PropTypes from 'prop-types';
+import { paramCase } from 'change-case';
 // ? replaced already
-import { Link as RouterLink } from "next/link";
+import { Link as RouterLink } from 'next/link';
 // material
-import { Box, Card, Link, Typography, Stack } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { Box, Card, Link, Typography, Stack } from '@mui/material';
+import { styled } from '@mui/material/styles';
 // routes
-import { PATH_DASHBOARD } from "src/otherComponents/routes/paths";
+import { PATH_DASHBOARD } from 'src/otherComponents/routes/paths';
 // utils
-import { fCurrency } from "src/otherComponents/utils/formatNumber";
+import { fCurrency } from 'src/otherComponents/utils/formatNumber';
 //
-import Label from "../../../Label";
-import axios from "axios";
-import Router from "next/router";
+import axios from 'axios';
+import Router from 'next/router';
+import Label from '../../../Label';
 // import ColorPreview from '../../../ColorPreview';
 
 // ----------------------------------------------------------------------
 
-const ProductImgStyle = styled("img")({
+const ProductImgStyle = styled('img')({
   top: 0,
-  width: "100%",
-  height: "100%",
-  objectFit: "contain",
-  position: "absolute",
+  width: '100%',
+  height: '100%',
+  objectFit: 'contain',
+  position: 'absolute',
 });
 
 // ----------------------------------------------------------------------
@@ -45,7 +45,7 @@ export default function ShopProductCard({ product, from, variants }) {
   } = product;
 
   console.log(
-    "🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄 This is from ShopProductCard.js, it is the product which is passed in from props and destructured, view at https://bit.ly/next12_18",
+    '🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄 This is from ShopProductCard.js, it is the product which is passed in from props and destructured, view at https://bit.ly/next12_18',
     product
   );
   // console.log(
@@ -53,7 +53,7 @@ export default function ShopProductCard({ product, from, variants }) {
   //   product.product.name
   // );
   console.log(
-    "🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄 ShopProductCard.js this is the make name: ",
+    '🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄 ShopProductCard.js this is the make name: ',
     product.car_make_name
   );
   // const makeName = product.product.name
@@ -65,63 +65,64 @@ export default function ShopProductCard({ product, from, variants }) {
   const linkToMakeName = `/dashboard/shop/${makeNameParamCase}`;
 
   const stringPrice = product.car_price;
-  console.log("This is the stringPrice: ", stringPrice);
+  console.log('This is the stringPrice: ', stringPrice);
   const intPrice = parseInt(stringPrice);
-  console.log("This is the intPrice: ", intPrice);
-  console.log("This is the price: ", price);
-  console.log("This is the product.image_url : ", product.image_url);
+  console.log('This is the intPrice: ', intPrice);
+  console.log('This is the price: ', price);
+  console.log('This is the product.image_url : ', product.image_url);
 
-  const removeElement = (arr, val) => {
-    return arr.filter((el) => el.id !== val);
-  };
+  const removeElement = (arr, val) => arr.filter((el) => el.id !== val);
 
   const onHeartPress = async () => {
-    let favouriteIds = variants.filter(item => item.isFavourite === true)
+    let favouriteIds = variants.filter((item) => item.isFavourite === true);
     // let notFavouriteIds = variants.filter(item => item.isFavourite === false)
 
-    if(product.isFavourite){
-      favouriteIds = removeElement(favouriteIds ,product.id)
-    }else {
-      favouriteIds.push({id: product.id})
+    if (product.isFavourite) {
+      favouriteIds = removeElement(favouriteIds, product.id);
+    } else {
+      favouriteIds.push({ id: product.id });
     }
 
-    axios.put("http://localhost:1337/users-favourites/61b30bbb3df9c32024fe7bd5", {
-      user: "61af216778c23d252fdceec2",
-      variants: favouriteIds.map(ele => ele.id)
-    })
-      .then(response => {
-        console.log('response ===>',response)
+    axios
+      .put('http://localhost:1337/users-favourites/61b30bbb3df9c32024fe7bd5', {
+        user: '61af216778c23d252fdceec2',
+        variants: favouriteIds.map((ele) => ele.id),
+      })
+      .then((response) => {
+        console.log('response ===>', response);
         Router.reload();
       })
-      .catch(err => {
-        console.log(err)
-      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <Card>
-      <img
-        src={`/static/icons/${
-          from === "favourite_page"
-            ? "heart_filled.svg"
-            : product.isFavourite
-            ? "heart_filled.svg"
-            : "heart_empty.svg"
-        }`}
-        className="heart-icon"
-        onClick={() => onHeartPress(product)}
-      />
-      <Box sx={{ pt: "100%", position: "relative" }}>
+      <Box width="10%" mx="1rem" my="0.5rem">
+        <img
+          src={`/static/icons/${
+            from === 'favourite_page'
+              ? 'heart_filled.svg'
+              : product.isFavourite
+              ? 'heart_filled.svg'
+              : 'heart_empty.svg'
+          }`}
+          className="heart-icon"
+          onClick={() => onHeartPress(product)}
+        />
+      </Box>
+      <Box sx={{ pt: '100%', position: 'relative' }}>
         {product.car_make_name && (
           <Label
             variant="filled"
-            color={(status === "sale" && "error") || "info"}
+            color={(status === 'sale' && 'error') || 'info'}
             sx={{
               top: 16,
               right: 16,
               zIndex: 9,
-              position: "absolute",
-              textTransform: "uppercase",
+              position: 'absolute',
+              textTransform: 'uppercase',
             }}
           >
             {product.vehicle_status}
@@ -144,8 +145,8 @@ export default function ShopProductCard({ product, from, variants }) {
                 component="span"
                 variant="body1"
                 sx={{
-                  color: "text.disabled",
-                  textDecoration: "line-through",
+                  color: 'text.disabled',
+                  textDecoration: 'line-through',
                 }}
               />
 
@@ -171,8 +172,8 @@ export default function ShopProductCard({ product, from, variants }) {
               component="span"
               variant="body1"
               sx={{
-                color: "text.disabled",
-                textDecoration: "line-through",
+                color: 'text.disabled',
+                textDecoration: 'line-through',
               }}
             >
               {priceSale && fCurrency(priceSale)}
